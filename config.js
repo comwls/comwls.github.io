f3080a199a39c7cf20a6ecef30433acdb3774ab9
comwls <comwls@naver.com>
2020-04-21 01:48:18 +0900

    config 파일 수정

--- config.js
+++ config.js
@@ -1,38 +1,38 @@
 module.exports = {
   /** Site MetaData (Required all)*/
-  title: `Title`,                           // (* Required)
-  description: `Blog Description`,          // (* Required)
-  author: `Author`,                         // (* Required)
-  siteUrl: 'https://',                      // (* Required)
+  title: `Dev.comwls`,                           // (* Required)
+  description: `comwls의 웹 개발 블로그입니다. 프론트엔드 와 PHP을 주로 다루며 백엔드와 그 외 기술에도 지속적으로 관심을 기울이고 있습니다.`, // (* Required)
+  author: `comwls`,                         // (* Required)
+  siteUrl: 'https://comwls.github.io',       // (* Required) 
     // ex.'https://junhobaik.github.io'
     // ex.'https://junhobaik.github.io/' << X, Do not enter "/" at the end.
 
   /** Header */
-  profileImageFileName: '', // include filename extension ex.'profile.jpg'
+  profileImageFileName: 'profile.png', // include filename extension ex.'profile.jpg'
     // The Profile image file is located at path "./images/"
     // If the file does not exist, it is replaced by a random image.
 
   /** Home > Bio information*/
-  comment: 'Enter your information in the "./config.js" file. ',
-  name: 'User Name',
+  comment: 'Web PHP && Front-end Developer. / laravel, javascript, react ...',
+  name: 'comwls',
   company: '',
   location: 'Korea',
-  email: '',
-  website: 'https://github.com/junhobaik/junhobaik.github.io',           // ex.'https://junhobaik.github.io'
-  linkedin: '',                                                          // ex.'https://www.linkedin.com/in/junho-baik-16073a19ab'
-  facebook: '',                                                          // ex.'https://www.facebook.com/zuck' or 'https://www.facebook.com/profile.php?id=000000000000000'
-  instagram: '',                                                         // ex.'https://www.instagram.com/junhobaik'
-  github: '',                                                            // ex.'https://github.com/junhobaik'
+  email: 'comwls@naver.com',
+  website: 'https://comwls.github.io',           // ex.'https://junhobaik.github.io'
+  linkedin: '',                                     // ex.'https://www.linkedin.com/in/junho-baik-16073a19ab'
+  facebook: '',                                     // ex.'https://www.facebook.com/zuck' or 'https://www.facebook.com/profile.php?id=000000000000000'
+  instagram: '', // ex.'https://www.instagram.com/junhobaik'
+  github: 'https://github.com/comwls',           // ex.'https://github.com/junhobaik'
 
   /** Post */
   enablePostOfContents: true,     // TableOfContents activation (Type of Value: Boolean. Not String)
-  disqusShortname: '',            // comments (Disqus sort-name)
+  disqusShortname: 'http-comwls-github-io',   // comments (Disqus sort-name)
   enableSocialShare: true,        // Social share icon activation (Type of Value: Boolean. Not String)
 
   /** Optional */
-  googleAnalytics: '',     // Google Analytics TrackingID. ex.'UA-123456789-0'
-  googleSearchConsole: '', // content value in HTML tag of google search console ownership verification. ex.'w-K42k14_I4ApiQKuVPbCRVV-GxlrqWxYoqO94KMbKo'
-  googleAdsenseSlot: '',   // Google Adsense Slot. ex.'5214956675'
-  googleAdsenseClient: '', // Google Adsense Client. ex.'ca-pub-5001380215831339'
+  googleAnalytics: '',                                  // Google Analytics TrackingID. ex.'UA-123456789-0'
+  googleSearchConsole: '', // content value in HTML tag of google search console ownership verification 
+  googleAdsenseSlot: '',                                    // Google Adsense Slot. ex.'5214956675'
+  googleAdsenseClient: '',                     // Google Adsense Client. ex.'ca-pub-5001380215831339'
     // Please correct the adsense client number(ex.5001380215831339) in the './static/ads.txt' file.
 };
