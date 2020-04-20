7a7725890132e8ec9c08185f42acff3ec1a4db01
comwls <comwls@naver.com>
2020-04-21 01:46:28 +0900

    첫 글 작성

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
   /** Site MetaData (Required all)*/
-  title: `Dev.White`,                           // (* Required)
-  description: `Dev.White의 웹 개발 블로그입니다. 프론트엔드 스킬을 주로 다루며 백엔드와 그 외 기술에도 지속적으로 관심을 기울이고 있습니다.`, // (* Required)
-  author: `Junho Baik`,                         // (* Required)
-  siteUrl: 'https://junhobaik.github.io',       // (* Required) 
+  title: `Dev.comwls`,                           // (* Required)
+  description: `comwls의 웹 개발 블로그입니다. 프론트엔드 와 PHP을 주로 다루며 백엔드와 그 외 기술에도 지속적으로 관심을 기울이고 있습니다.`, // (* Required)
+  author: `comwls`,                         // (* Required)
+  siteUrl: 'https://comwls.github.io',       // (* Required) 
     // ex.'https://junhobaik.github.io'
     // ex.'https://junhobaik.github.io/' << X, Do not enter "/" at the end.
 
@@ -13,26 +13,26 @@ module.exports = {
     // If the file does not exist, it is replaced by a random image.
 
   /** Home > Bio information*/
-  comment: 'Jr. Web Front-end Developer. / javascript, react ...',
-  name: 'Junho Baik',
+  comment: 'Web PHP && Front-end Developer. / laravel, javascript, react ...',
+  name: 'comwls',
   company: '',
   location: 'Korea',
-  email: 'junhobaik@gmail.com',
-  website: 'https://junhobaik.github.io',           // ex.'https://junhobaik.github.io'
+  email: 'comwls@naver.com',
+  website: 'https://comwls.github.io',           // ex.'https://junhobaik.github.io'
   linkedin: '',                                     // ex.'https://www.linkedin.com/in/junho-baik-16073a19ab'
   facebook: '',                                     // ex.'https://www.facebook.com/zuck' or 'https://www.facebook.com/profile.php?id=000000000000000'
-  instagram: 'https://www.instagram.com/junhobaik', // ex.'https://www.instagram.com/junhobaik'
-  github: 'https://github.com/junhobaik',           // ex.'https://github.com/junhobaik'
+  instagram: '', // ex.'https://www.instagram.com/junhobaik'
+  github: 'https://github.com/comwls',           // ex.'https://github.com/junhobaik'
 
   /** Post */
   enablePostOfContents: true,     // TableOfContents activation (Type of Value: Boolean. Not String)
-  disqusShortname: 'junhobaik',   // comments (Disqus sort-name)
+  disqusShortname: 'http-comwls-github-io',   // comments (Disqus sort-name)
   enableSocialShare: true,        // Social share icon activation (Type of Value: Boolean. Not String)
 
   /** Optional */
-  googleAnalytics: 'UA-103592668-4',                                  // Google Analytics TrackingID. ex.'UA-123456789-0'
-  googleSearchConsole: 'w-K42k14_I4ApiQKuVPbCRVV-GxlrqWxYoqO94KMbKo', // content value in HTML tag of google search console ownership verification 
-  googleAdsenseSlot: '5214956675',                                    // Google Adsense Slot. ex.'5214956675'
-  googleAdsenseClient: 'ca-pub-5001380215831339',                     // Google Adsense Client. ex.'ca-pub-5001380215831339'
+  googleAnalytics: '',                                  // Google Analytics TrackingID. ex.'UA-123456789-0'
+  googleSearchConsole: '', // content value in HTML tag of google search console ownership verification 
+  googleAdsenseSlot: '',                                    // Google Adsense Slot. ex.'5214956675'
+  googleAdsenseClient: '',                     // Google Adsense Client. ex.'ca-pub-5001380215831339'
     // Please correct the adsense client number(ex.5001380215831339) in the './static/ads.txt' file.
 };
